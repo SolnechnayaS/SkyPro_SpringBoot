505ee6b74e24a288592a25f92f1a473ea97ef75b
Svetlana Morozova <svetlanochka@yandex.ru>
2023-07-05 22:09:17 +0300

    HomeWork290623

--- src/main/java/org/ru/skypro/lessons/spring/EmployeeApplication/security/UserRepository.java
+++ src/main/java/org/ru/skypro/lessons/spring/EmployeeApplication/security/UserRepository.java
@@ -4,8 +4,5 @@ import org.ru.skypro.lessons.spring.EmployeeApplication.security.AuthUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<AuthUser, Long> {
-
-    // Создаем метод findByUsername
-    // для поиска пользователя по имени пользователя
     AuthUser findByUsername(String username);
 }
